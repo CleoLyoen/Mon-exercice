62b8d5e8f1fef1f048e0335b6b82b4e03aa32106
Cleol <cleo.lyoen@gmail.com>
2022-09-19 15:11:54 +0200

    premier commit 1

---
+++ script.js
@@ -0,0 +1,28 @@
+var ville= "Cambrai"
+const hypotenuse=8
+var admission="vrai"
+var nom="Lyoen"
+var prénom="Cleo"
+var âge=32
+var pomme=8
+var pêches=6
+var totalfruits=pomme*pêches
+console.log(totalfruits)
+var toto=10
+var titi=20
+var totaltototiti=toto/titi
+console.log(totaltototiti)
+function multiplication (a,b) {
+return a*b
+}
+
+console.log (multiplication(4,6))
+
+const person = { 
+
+    firstName: " Lyoen",
+    LastName: "Attention ",
+    Naissance : "Cambrai"
+};
+
+alert(person.LastName+person.Naissance+person.firstName)
